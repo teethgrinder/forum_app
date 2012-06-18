08f5f45c3756dd45c61f078790923524b7724d26
Burak AKIN <burak.akin@gmail.com>
2012-06-18 18:18:00 +0300

    Add a StaticPages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForumApp::Application.config.secret_token = 'ca6e95b39aec6ca50851b309a749f37867153fbc8b331120077fc37d4da8f722f9d695eb5e397b1a83d95818d7651b1264386728c65e1bde9d7ff9967f13788d'
